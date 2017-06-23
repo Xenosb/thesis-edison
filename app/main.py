bf3cc842830d277ae9bed1fe52d6d6d7f3804fae
Bruno Vunderl <brunovu@gmail.com>
2017-06-23 21:48:40 +0200

    Added first template

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 application = Flask(__name__)
 
-@application.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
 
-if __name__ == "__main__":
-    application.run(host='0.0.0.0')
+@application.route('/')
+def hello():
+     return render_template('hello.html', name='Lucija')
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0', debug='True')
