1c6c45d03487cc2b4d956b7d0173aeaf24196595
Bruno Vunderl <brunovu@gmail.com>
2017-06-27 15:46:42 +0200

    AJAX intervaled reload of sensor values

--- app/static/js/views/index.js
+++ app/static/js/views/index.js
@@ -1,3 +1,26 @@
-$(document).ready(function($){
-  sensor_refresher = setInterval(ajax_refresh_sensor_values, 300);
+$(document).ready(function ($) {
+  sensor_refresher = setInterval(ajax_refresh_sensor_values, 1000);
 });
+
+function ajax_set_sensor_value(data) {
+  $("#s1-val").text(data.result.last_value);
+}
+
+function ajax_get_sensor_value(sensor_id) {
+  sensor_url = 'api/sensor?id=' + sensor_id;
+  sensor_div = $('#s' + sensor_id + '-val')
+
+  $.ajax({
+    url: sensor_url,
+    type: "GET",
+    data: {'id': sensor_id},
+    dataType: 'json',
+    success: function (data) {
+      sensor_div.text(data.result.last_value);
+    }
+  });
+}
+
+function ajax_refresh_sensor_values() {
+  ajax_get_sensor_value(1);
+}
