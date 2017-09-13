e7a724123974fdadc9a6764e4768d193257da54b
Bruno Vunderl <brunovu@gmail.com>
2017-09-13 08:19:01 +0000

    Removed client production configuration

--- config.py
+++ config.py
@@ -2,25 +2,21 @@ class Config(object):
   DEBUG = True
   EDISON = False
   SECRET_KEY = 'this-really-needs-to-be-changed'
-  SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/smartbed'
+  SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/smartbed-test'
   UPDATE_FREQ = 0.5
 
 class EdisonProductionConfig(Config):
-  DEBUG = False
+  DEBUG = True
   EDISON = True
+  SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/smartbed-prod'
 
 class EdisonTestConfig(Config):
   DEBUG = True
   EDISON = True
 
-class ClientProductionConfig(Config):
-  DEBUG = False
-  EDISON = False
-  SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/smartbed-prod'
-
 class ClientTestConfig(Config):
   DEBUG = True
   EDISON = False
-  SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/smartbed-test'
 
-config_s = ClientTestConfig
+config_s = EdisonTestConfig
+#config_s = ClientTestConfig
